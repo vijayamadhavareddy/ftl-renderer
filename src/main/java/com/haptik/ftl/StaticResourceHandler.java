3c2e22f9a68fc1d3a5d66b1bacff740c041ae1ae
Vijay D <vijayd@trianabot.com>
2021-08-06 13:57:37 +0530

    JS and CSS update

--- src/main/java/com/haptik/ftl/StaticResourceHandler.java
+++ src/main/java/com/haptik/ftl/StaticResourceHandler.java
@@ -11,38 +11,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class StaticResourceHandler implements WebMvcConfigurer {
 
-    private final String jsFolderPath;
-    private final String cssFolderPath;
+    private final String staticAssetsPath;
 
     @Autowired
-    public StaticResourceHandler(@Value("${static.folder.js}") String jsFolderPath, @Value("${static.folder.css}") String cssFolderPath) {
+    public StaticResourceHandler(@Value("${static.folder}") String staticAssetsPath) {
         boolean isWindows = System.getProperty("os.name").startsWith("Windows");
-        if (jsFolderPath == null || jsFolderPath.isEmpty()) {
+        if (staticAssetsPath == null || staticAssetsPath.isEmpty()) {
             if (isWindows) {
-                this.jsFolderPath = String.format("file:///%s/templates/static/js/", System.getProperty("user.dir"));
+                this.staticAssetsPath = String.format("file:///%s/templates/static/", System.getProperty("user.dir"));
             } else {
-                this.jsFolderPath = String.format("file:%s/templates/static/js/", System.getProperty("user.dir"));
+                this.staticAssetsPath = String.format("file:%s/templates/static/", System.getProperty("user.dir"));
             }
         } else {
-            this.jsFolderPath = jsFolderPath;
-        }
-        if (cssFolderPath == null || cssFolderPath.isEmpty()) {
-            if (isWindows) {
-                this.cssFolderPath = String.format("file:///%s/templates/static/css/", System.getProperty("user.dir"));
-            } else {
-                this.cssFolderPath = String.format("file:%s/templates/static/css/", System.getProperty("user.dir"));
-            }
-        } else {
-            this.cssFolderPath = cssFolderPath;
+            this.staticAssetsPath = staticAssetsPath;
         }
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        WebMvcConfigurer.super.addResourceHandlers(registry);
-        registry.addResourceHandler("/ftl/static/js/**")
-                .addResourceLocations(jsFolderPath);
-        registry.addResourceHandler("/ftl/static/css/**")
-                .addResourceLocations(cssFolderPath);
+        registry.addResourceHandler("/ftl/static/**")
+                .addResourceLocations(staticAssetsPath);
     }
 }
